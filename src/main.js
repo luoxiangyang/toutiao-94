a0e9541112637ff0ef111eb7473b105a59733dcd
Programmer-new <2585710570@qq.com>
2020-02-29 15:42:27 +0800

    初始化项目-整理目录-准备资源

--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@ import App from './App.vue'
 import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'// 引入样式
+import '@/styles/index.less'
 Vue.config.productionTip = false
 Vue.use(ElementUI) // 全局注册
+// Vue.use() 会调用ElementUI对象的一个方法  install方法传入当前VUe对象
 new Vue({
   router,
   render: h => h(App)

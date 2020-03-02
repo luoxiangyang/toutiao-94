39f15528eacb823a2e0f51a6904264cc635e3390
Programmer-new <2585710570@qq.com>
2020-03-02 14:20:04 +0800

    表单校验与axios配置

--- src/main.js
+++ src/main.js
@@ -4,9 +4,13 @@ import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'// 引入样式
 import '@/styles/index.less'
+import axios from 'axios'
 Vue.config.productionTip = false
 Vue.use(ElementUI) // 全局注册
 // Vue.use() 会调用ElementUI对象的一个方法  install方法传入当前VUe对象
+// 配置axios得baseURl
+axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0'
+Vue.prototype.$axios = axios // 将axios赋值给vue全局对象的原型属性
 new Vue({
   router,
   render: h => h(App)
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -8,33 +8,82 @@
         <img src="../../assets/img/logoOW.png" alt />
       </div>
       <!-- 表单 -->
-      <el-form style="margin-top:20px">
-        <!-- 表单容器 -->
-        <el-form-item>
-          <!-- 表单域 -->
-          <el-input placeholder="请输入手机号"></el-input>
+      <el-form :model="loginForm" :rules="loginRules" ref="loginForm" style="margin-top:20px">
+        <!-- 表单容器 prop 表示要校验的字段名-->
+        <el-form-item prop="mobile">
+          <!-- 表单域 双向绑定-->
+          <el-input v-model="loginForm.mobile" placeholder="请输入手机号"></el-input>
         </el-form-item>
-        <el-form-item>
-          <el-input style="width:60%" placeholder="请输入验证码"></el-input>
+        <el-form-item prop="code">
+          <el-input v-model="loginForm.code" style="width:60%" placeholder="请输入验证码"></el-input>
           <!-- 防止按钮 -->
           <el-button plain style="float:right">发送验证码</el-button>
         </el-form-item>
-        <el-form-item>
+        <el-form-item prop="checked">
           <!-- 是否同意 -->
-          <el-checkbox style="color:#000">
+          <el-checkbox v-model="loginForm.checked" style="color:#000">
             我已阅读同意
             <a href="#" style="color:blue">用户协议</a> 和
             <a href="#" style="color:blue">隐私条款</a>
           </el-checkbox>
         </el-form-item>
         <el-form-item>
-          <el-button type="primary" style="width:100%">登录</el-button>
+          <el-button @click="login" type="primary" style="width:100%">登录</el-button>
         </el-form-item>
       </el-form>
     </el-card>
   </div>
 </template>
+<script>
+export default {
+  data () {
+    return {
+      // 登录表单的数据
+      loginForm: {
+        mobile: '', // 手机号
+        code: '', // 验证码
+        checked: false // 是否同意
+      },
+      // 表单的验证规则
+      loginRules: {
+        // required 如果为true 就为必填
+        // message
+        mobile: [
+          { required: true, message: '您手机号不能为空' },
+          { pattern: /^1[3-9]\d{9}$/, message: '您的手机号格式不正确' }
+        ], // 手机号验证规则
+        code: [
+          { required: true, message: '您验证码不能为空' },
+          { pattern: /^\d{6}$/, message: '验证码为6位' }
+        ], // 验证码验证规则
+        checked: [
+          {
+            validator: function (rule, value, callback) {
+              // rule当前校验规则
+              // value 是当前的要校验的字段
+              // callback是一个回调函数 不论成功失败都执行 callback(new Error)
+              value ? callback() : callback(new Error('必须同意阅读'))
+            }
+          }
+        ] // 是否同意验证规则
+      }
 
+    }
+  },
+  methods: {
+    login () {
+      // ref 可以获取dom对象  还可以获取组件对象实例
+      this.$refs.loginForm.validate((isOk) => {
+        if (isOk) {
+          // 表示校验通过
+        } else {
+          // 校验未通过
+        }
+      })
+    }
+  }
+}
+</script>
 <style lang="less" scoped>
 .login {
   background-image: url("../../assets/img/login_bg2.jpg");
